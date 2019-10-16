489e38c24c3fb95cc532808b14117eee01598ea6
Administrator <kamalhajari123@gmail.com>
2019-10-16 13:46:25 +0530

    kamal

--- app/src/main/java/com/dmims/dmims/remote/IMyAPI.kt
+++ app/src/main/java/com/dmims/dmims/remote/IMyAPI.kt
@@ -1,5 +1,6 @@
 package com.dmims.dmims.remote
 
+import com.dmims.dmims.activity.Task
 import com.dmims.dmims.model.APIResponse
 import org.json.JSONObject
 import retrofit2.Call
@@ -101,9 +102,9 @@ interface IMyAPI {
     @POST("Login/StudentSearchByRollNo")
     fun StudentSearchByRollNo(@Field("roll_no") roll_no: String, @Field("course_id")course_id:String): Call<APIResponse>
 
-    @FormUrlEncoded
+
     @POST("Feedback/Feedback_Post")
-    fun SubmitExamFeedback(@Field ("jsonstring")jsonstring: String): Call<APIResponse>
+    fun SubmitExamFeedback(@Body task: Task): Call<APIResponse>
 
 //    fun SubmitExamFeedback(@Field ("FEEDBACK_TYPE")FEEDBACK_TYPE: String,
 //                           @Field ("COURSE_ID")COURSE_ID: String,
